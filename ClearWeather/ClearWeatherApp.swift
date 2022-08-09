34a390ecc6dcbfe1264a8b37f6fd8c1884dd3d9d
Daniel Teodoro <daniel.missimat@gmail.com>
2022-08-08 23:27:09 -0300

    first commit

---
+++ ClearWeather/ClearWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClearWeatherApp.swift
+//  ClearWeather
+//
+//  Created by Daniel Teodoro on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ClearWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
